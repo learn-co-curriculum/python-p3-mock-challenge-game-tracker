60c40b3e59558d357762e96a132601776638f451
Matteo Piccini <gnappo.1@live.it>
2023-08-02 08:14:37 -0700

    Update tests and README to be consistent with other mocks

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -1,14 +1,15 @@
 class Game:
     def __init__(self, title):
         self.title = title
-        
+
     def results(self):
         pass
-    
+
     def players(self):
         pass
-    
+
     def average_score(self, player):
         pass
+
 
 from classes.result import Result
--- lib/classes/player.py
+++ lib/classes/player.py
@@ -1,25 +1,18 @@
 class Player:
-
-    all = []
-
     def __init__(self, username):
         self.username = username
-        
+
     def results(self):
         pass
-    
+
     def games_played(self):
         pass
-    
+
     def played_game(self, game):
         pass
-    
+
     def num_times_played(self, game):
         pass
-    
-    @classmethod
-    def highest_scored(cls, game):
-        pass
-        
+
 
 from classes.result import Result
